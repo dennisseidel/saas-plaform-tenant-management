ea2cd20e84823ea31b35f2df85813c9a9d18c531
Dennis Seidel <den.seidel@gmail.com>
2019-06-26 11:35:47 +0200

    chore: get userid for tenant admin from sub

--- service/handler.ts
+++ service/handler.ts
@@ -1,10 +1,8 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import { DynamoDB } from 'aws-sdk';
-
-
-const uuid = require('uuid');
-// const jwtDecode = require('jwt-decode');
+import { v1 } from 'uuid';
+import * as jwtDecode from 'jwt-decode';
 
 const config = {
   tenant_management_db_name: process.env.tenant_management_db_name
@@ -12,32 +10,34 @@ const config = {
 
 const dynamoDb = new DynamoDB.DocumentClient();
 
-// const getSub = (event) => {
-//   let sub = '';
-//   const AuthorizationHeader = event.headers.Authorization;
-//   if (AuthorizationHeader) {
-//     bearerToken = AuthorizationHeader.substring(AuthorizationHeader.indexOf(' ') + 1);
-//     var decodedAccessToken = jwtDecode(bearerToken);
-//     if (decodedAccessToken)
-//       sub = decodedAccessToken['sub'];
-//   }
-//   return sub;
-// }
+const getSub = (authorizationHeader) => {
+  let sub = '';
+  if (authorizationHeader) {
+    const bearerToken = authorizationHeader.substring(authorizationHeader.indexOf(' ') + 1);
+    const decodedAccessToken = jwtDecode(bearerToken);
+    if (decodedAccessToken)
+      sub = decodedAccessToken['sub'];
+  }
+  return sub;
+}
 
 
 export const createTenant: APIGatewayProxyHandler = async (event, _context) => {
   const { plan, tenantName }: { plan: string, tenantName: string } = JSON.parse(event.body)
-  const headers = {
+  const authorizationHeader = event.headers.Authorization;
+
+  const responseHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": true
   }
+  const sub = getSub(authorizationHeader)
   const params = {
     TableName: config.tenant_management_db_name,
     Item: {
-      tenantId: uuid.v1(),
+      tenantId: v1(),
       tenantName: tenantName,
       plan: plan,
-      userId: 'requestorSub',
+      userId: sub,
       role: 'tenant-admin',
       createdAt: Date.now()
     }
@@ -46,7 +46,7 @@ export const createTenant: APIGatewayProxyHandler = async (event, _context) => {
     await dynamoDb.put(params).promise();
     return {
       statusCode: 200,
-      headers: headers,
+      headers: responseHeaders,
       body: JSON.stringify(params.Item)
     }
   }
@@ -54,7 +54,7 @@ export const createTenant: APIGatewayProxyHandler = async (event, _context) => {
     console.log(error)
     return {
       statusCode: 500,
-      headers: headers,
+      headers: responseHeaders,
       body: JSON.stringify({ "status": false })
     };
   }
