c0af1c9e891dbb1e7798c65b66fea7dce0cb347b
Dennis Seidel <den.seidel@gmail.com>
2019-06-29 15:33:26 +0200

    feat: simplify tenant overview

--- ui/src/components/TenantOverviewTable/TenantOverviewTable.tsx
+++ ui/src/components/TenantOverviewTable/TenantOverviewTable.tsx
@@ -1,10 +1,6 @@
-import { List, message, Avatar, Spin } from "antd";
-import reqwest from "reqwest";
+import { List, message, Avatar, Spin, Tag } from "antd";
+import axios from "axios";
 import React from "react";
-
-import InfiniteScroll from "react-infinite-scroller";
-import { access } from "fs";
-import { inflateRaw } from "zlib";
 
 interface ITenantOverviewProps {
   children?: React.ReactChild;
@@ -23,34 +19,11 @@ type tenant = {
   plan: string;
   tenantId: string;
   createdAt: number;
+  role: string;
 };
-
-const fakeDataUrl =
-  "https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo";
 
 const tenantDataUrl =
   "https://2wifthija8.execute-api.eu-central-1.amazonaws.com/dev/tenants";
-
-const tenantData = {
-  tenants: [
-    {
-      tenantName: "NovaTeam2",
-      plan: "premium",
-      createdAt: 1561636461699,
-      role: "tenant-admin",
-      userId: "115c128e-ecf8-41ba-b6ff-8c419d2ce21f",
-      tenantId: "4d2efd30-98d2-11e9-b698-81fb57414177"
-    },
-    {
-      tenantName: "NovaTeam3",
-      plan: "premium",
-      createdAt: 1561636462676,
-      role: "tenant-admin",
-      userId: "115c128e-ecf8-41ba-b6ff-8c419d2ce21f",
-      tenantId: "4dc41140-98d2-11e9-b698-81fb57414177"
-    }
-  ]
-};
 
 class InfiniteListExample extends React.Component<
   ITenantOverviewProps,
@@ -58,7 +31,6 @@ class InfiniteListExample extends React.Component<
 > {
   constructor(props: ITenantOverviewProps) {
     super(props);
-    // Don't call this.setState() here!
     this.state = {
       data: [],
       loading: false,
@@ -68,79 +40,49 @@ class InfiniteListExample extends React.Component<
   }
 
   componentDidMount() {
-    this.fetchData(res => {
-      this.setState({
-        data: tenantData.tenants
-      });
-    });
+    this.fetchData();
   }
 
-  fetchData = callback => {
-    reqwest({
-      url: fakeDataUrl,
-      type: "json",
-      method: "get",
-      contentType: "application/json",
-      success: res => {
-        callback(res);
+  fetchData = async () => {
+    const res: {
+      data: {
+        tenants: tenant[];
+      };
+    } = await axios.get(tenantDataUrl, {
+      headers: {
+        Authorization: `Bearer ${this.state.access_token}`
       }
     });
-  };
-
-  handleInfiniteOnLoad = () => {
-    let { data } = this.state;
+    console.log(res);
     this.setState({
-      loading: true
-    });
-    if (data.length > 14) {
-      message.warning("Infinite List loaded all");
-      this.setState({
-        hasMore: false,
-        loading: false
-      });
-      return;
-    }
-    this.fetchData(res => {
-      data = data.concat(res.results);
-      this.setState({
-        data,
-        loading: false
-      });
+      data: res.data.tenants
     });
   };
 
   render() {
     return (
       <div className="demo-infinite-container">
-        <InfiniteScroll
-          initialLoad={false}
-          pageStart={0}
-          loadMore={this.handleInfiniteOnLoad}
-          hasMore={!this.state.loading && this.state.hasMore}
-          useWindow={false}
-        >
-          <List
-            dataSource={this.state.data}
-            renderItem={(item: tenant) => (
-              <List.Item key={item.tenantId}>
-                <List.Item.Meta
-                  avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                  }
-                  title={<a href="https://ant.design">{item.tenantName}</a>}
-                  description={item.tenantId}
-                />
-                <div>Content</div>
-              </List.Item>
-            )}
-          >
-            {this.state.loading && this.state.hasMore && (
-              <div className="demo-loading-container">
-                <Spin />
+        <List
+          dataSource={this.state.data}
+          renderItem={(item: tenant) => (
+            <List.Item
+              key={item.tenantId}
+              actions={[<a>manage users</a>, <a>delete</a>]}
+            >
+              <List.Item.Meta
+                avatar={
+                  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                }
+                title={<a href="https://ant.design">{item.tenantName}</a>}
+                description={item.tenantId}
+              />
+              <div>
+                <Tag color="red">{item.role}</Tag>
+                <Tag color="green">{item.plan}</Tag>
               </div>
-            )}
-          </List>
-        </InfiniteScroll>
+            </List.Item>
+          )}
+        />
       </div>
     );
   }
